f5dc9ce73d3b3713cbb40af496b5f399b9ebcd02
Sanghoon Shin <sanghoonshin@Orca-MacBook.local>
2020-08-20 18:17:54 -0500

    props

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,8 +3,22 @@ import React, { Component } from "react";
 // 다른 component 와 소통할 수 있도록 단순한 함수가 아니라 class 로 선언.
 // class 선언할 때는 render method 와 return 값을 줘야함
 class SearchBar extends Component {
+  // functional components 는 state 가 없다. Only class-bassed components
+  // constructor 는 class 의 new instance 가 생성될 때마다 호출되는 함수. Initializing variables & state 등등
+  constructor(props) {
+    super(props); // parent class 에서 정의된 method 를 정의할 때 (Component) super 사용
+
+    this.state = { term: " " }; // component 를 update 하고 싶으면 state 을 떠올려라
+  }
   render() {
-    return <input onChange={(event) => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={(event) => this.setState({ term: event.target.value })}
+        />
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDom from "react-dom";
+// import dotenv from "react-native-dotenv";
+// dotenv.config();
+
+import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
 
 const API_KEY = process.env.API_KEY;
 
 // Create a new component. This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+    YTSearch({ key: API_KEY, term: "surfboards" }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 
 // Take this component's HTML and put it on the page (in the DOM)
 ReactDom.render(<App />, document.querySelector(".container")); // App 은 클래스, <App /> 인스턴스 호출
