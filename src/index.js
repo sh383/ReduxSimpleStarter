3cb75391329cb2608ab1d16775f0919e27d92ce9
Sanghoon Shin <sanghoonshin@Orca-MacBook.local>
2020-08-24 00:38:18 -0500

    fetching videos & video detail & video selection

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import React, { Component } from "react";
 import ReactDom from "react-dom";
-// import dotenv from "react-native-dotenv";
-// dotenv.config();
 
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
 const API_KEY = process.env.API_KEY;
-
 // Create a new component. This component should produce some HTML
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
-    YTSearch({ key: API_KEY, term: "surfboards" }, (videos) => {
-      this.setState({ videos });
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+
+    YTSearch({ key: API_KEY, term: "surfboard" }, (videos) => {
+      this.setState({ videos: videos, selectedVideo: videos[0] });
     });
   }
 
@@ -23,7 +26,12 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        {/* JSX 태그를 이용하여 videos list 를 전달 */}
+        <VideoList
+          onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
