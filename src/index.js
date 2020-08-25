9e9fc7cfabf5f19ab56545eb9d3e512031ea0b96
Sanghoon Shin <sanghoonshin@Orca-MacBook.local>
2020-08-24 23:35:35 -0500

    Searching

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import React, { Component } from "react";
 import ReactDom from "react-dom";
 
@@ -17,15 +18,23 @@ class App extends Component {
       selectedVideo: null,
     };
 
-    YTSearch({ key: API_KEY, term: "surfboard" }, (videos) => {
+    this.videoSearch("surfboards");
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
       this.setState({ videos: videos, selectedVideo: videos[0] });
     });
   }
 
   render() {
+    const videoSearch = _.debounce((term) => {
+      this.videoSearch(term), 300;
+    });
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         {/* JSX 태그를 이용하여 videos list 를 전달 */}
         <VideoList
